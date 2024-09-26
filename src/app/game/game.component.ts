38f4b3beb7c203df6656ee96954f7409858b1b34
Maximilian Seebacher <m.seebacher@students.htl-leonding.ac.at>
2024-09-26 16:10:10 +0200

    Completed Tic Tac Toe

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css'],
+  standalone: true,
+  imports: [CommonModule]
+})
+export class GameComponent {
+  squares: any[] = [];
+  xIsNext: boolean = true;
+  winner: string | null = null;
+
+  constructor() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  makeMove(idx: number) {
+    if (!this.squares[idx] && !this.winner) {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+      this.winner = this.calculateWinner();
+    }
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (this.squares[a] && this.squares[a] === this.squares[b] && this.squares[a] === this.squares[c]) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+}
